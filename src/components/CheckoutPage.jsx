497bb2258061070ea4f3052b4dbc9665478a1926
ghulamnabi-developer <studentdeveloper78@gmail.com>
2024-10-04 12:26:02 +0500

    some change in checkoutpage

--- src/components/CheckoutPage.jsx
+++ src/components/CheckoutPage.jsx
@@ -1,9 +1,8 @@
-// src/components/CheckoutPage.js
 import React, { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { loadStripe } from '@stripe/stripe-js';
 
-const stripePromise = loadStripe('pk_test_51Q2rwVGBop2eLpBQIem0BojnX3wajyG8eIddug3sRwikAXxXqHU2RK8COacViRmOGZbIBJyM2ZXBjyMafthY9RWC009V5C5quN'); 
+const stripePromise = loadStripe('pk_test_51Q2rwVGBop2eLpBQIem0BojnX3wajyG8eIddug3sRwikAXxXqHU2RK8COacViRmOGZbIBJyM2ZXBjyMafthY9RWC009V5C5quN');
 
 const CheckoutPage = () => {
   const location = useLocation();
@@ -21,18 +20,26 @@ const CheckoutPage = () => {
       };
 
       const expressCheckoutOptions = {
-        paymentMethods:{applePay:"always",googlePay:"always",amazonPay:"auto"},
+        paymentMethods: {
+          applePay: "always",
+          googlePay: "always",
+          amazonPay: "auto"
+        },
         buttonHeight: 50,
         buttonTheme: {
-          applePay: 'white-outline',
-        //   googlePay:'always'
+          applePay: 'white-outline'
+        },
+        style: {
+          base: {
+            width: '300px',  // Set width
+          }
         }
-      }
+      };
 
       const elements = stripe.elements({
         locale: 'de',
         mode: 'payment',
-        amount: totalAmount * 100,  
+        amount: totalAmount * 100,
         currency: 'usd',
         appearance,
       });
@@ -51,9 +58,8 @@ const CheckoutPage = () => {
     <div className="container mx-auto mt-8 p-4 bg-gray-100 rounded-lg shadow-lg">
       <h1 className="text-3xl font-semibold mb-4 text-center">Checkout</h1>
       <p className="text-center text-xl mb-4">Total Amount: ${totalAmount.toFixed(2)}</p>
-      <div id="express-checkout-element"></div>
+      <div id="express-checkout-element" className="mx-auto"></div> {/* Add 'mx-auto' for centering */}
       <div className="mt-6 text-center">
-    
         <button 
           className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-600 transition duration-200 mx-2"
           onClick={() => navigate('/')}
